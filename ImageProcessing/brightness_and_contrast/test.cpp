9aa533c6837aff983d8ca8ae3371b121e0802061
Alextz307 <alexofficialadress7@gmail.com>
2024-04-03 13:57:22 +0300

    Added testing for brightness and contrast

--- ImageProcessing/brightness_and_contrast/test.cpp
+++ ImageProcessing/brightness_and_contrast/test.cpp
@@ -3,6 +3,16 @@
 #include "brightness_and_contrast.h"
 
 int main() {
+    Image img;
+    img.load("../../assets/baboon.ascii.pgm");
+
+    BrightnessAndContrast processor = BrightnessAndContrast(1.5, 1.25);
+
+    Image dst;
+
+    processor.process(img, dst);
+
+    dst.save("../../assets/new_processor_images/brightness_and_contrast.ascii.pgm");
 
     return 0;
 }
